f462a2c3ee0b66bf02f374ead2d1b96a2be2cb70
Tom Victor <vjtomvictor@icloud.com>
2024-02-10 08:45:52 +0100

    SKILL-8: complete data fetching from the API

--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -1,9 +1,9 @@
-export default function User({id,firstName,lastName}) {
+export default function User({id,fullName,email}) {
   return (
     <tr>
         <th scope="row">{id}</th>
-        <td>{firstName}</td>
-        <td>{lastName}</td>
+        <td>{fullName}</td>
+        <td>{email}</td>
         <td>
           <span class="badge rounded-pill text-bg-primary">Go</span>
           <span class="badge rounded-pill text-bg-secondary">Python</span>
